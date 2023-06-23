fd6135248fc8d32fc0bba99417441d8166600169
komewel <kimcomewel@gmail.com>
2023-06-23 16:22:43 +0900

    1

--- login.js
+++ login.js
@@ -1,6 +1,7 @@
 var idPass = false;
 var pwPass = false;
 $('#id').on('keyup', function(){
+// alert($(this).val()); // $(this).val() == this.value
 let idValue = $(this).val();
 if(idValue.length == 0){
 $('#msg_id').text('');
@@ -12,7 +13,9 @@ idPass = false;
 $('#msg_id').text('정상적인 아이디입니다.').css('color', '#0000FF').css('font-size', '14px');
 idPass = true;
 }
+// idPass = idValue.length >= 4;
 })
+
 $('#frm_login').on('submit', function(event){
 if(idPass == false){
 alert('아이디를 확인하세요.');
